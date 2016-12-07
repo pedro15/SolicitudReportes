d41f99c1fade8108667e799685eab9e589d93f35
pedro15 <pedrojosuedm@hotmail.com>
2016-12-07 00:54:14 -0400

    Optimizando formulario vista

--- application/views/app/v_admintec.php
+++ application/views/app/v_admintec.php
@@ -32,7 +32,7 @@
     </form>
 
     <div class="table-responsive" >
-        <table class="table table-striped" id = "tablefill" >
+        <table class="table table-striped">
             <thead>
                 <tr>
                     <th>Cedula</th>
@@ -44,112 +44,105 @@
                 
             </tbody>
         </table>
-       
-        <script type = "text/javascript">
-            
-            var m_json = "";
-            
-            $(document).ready(function()
-            {
-                updatetable();
-            });
-
-            $("#ch_name").change(function()
-            {
-                updatetable();
-            });
-
-            $("#ch_ci").change(function()
-            {
-                updatetable();
-            });
-
-            $("#txt_name").keyup(function()
-            {
-                updatetable();
-            });
-
-            $("#txt_ci").keyup(function()
-            {
-                updatetable();
-            });
-
-            function validate_delete()
-            {
-                return confirm("Desea eliminar este registro?");
-            }
-            
-            function updatetable()
-            {
-                $.ajax
-                (
-                    {
-                        type : "POST",
-                        url: "<?php echo base_url('index.php/user/getalltecs'); ?>",
-                        datatype : 'json', 
-                        data: {} ,
-                        success:
-                        function (res)
-                        {
-                            m_json = JSON.parse(res);
-                            var name_checked = $("#ch_name").is(':checked');
-                            if (name_checked)
-                            {
-                                var f_arr = m_json.filter(function (value)
-                                {
-                                    var filtrer_name = $("#txt_name").val();
-                                    var x = value.nombre.includes(filtrer_name);
-                                    return x ;
-                                });
-                                m_json = f_arr;
-                            }
-
-                            var ci_checked = $("#ch_ci").is(":checked");
-                            if (ci_checked)
-                            {
-                                var m_arr = m_json.filter(function (value)
-                                {
-                                    var filter_ci = $("#txt_ci").val();
-                                    var x = value.cedula_usuario.includes(filter_ci);
-                                    return x;
-                                });
-                                m_json = m_arr;
-                            }
-
-                            populate(m_json);
-                        }
-                    }
-                );
-            }
-
-            function populate(xjson , isfiltrer)
-            {
-                updatehtml(xjson);
-                $("#tablefill").pagination
-                ({
-                        dataSource: xjson,
-                        pageSize : 20,
-                        callback: function(data, pagination) 
-                        {
-                           xjson = data;
-                           updatehtml(xjson);
-                        }
-                });
-            }
-
-            function updatehtml(xjson)
-            {
-                var _html = "" ;
-                for (data in xjson)
-                {
-                    _html += "<tr><th>" + xjson[data].cedula_usuario + "</th>" + 
-                    "<th>" + xjson[data].nombre + "</th>" +
-                    "<th>" + xjson[data].correo + "</th>" +
-                    "<th>" + '<a class = "btn btn-primary" href = "<?php echo base_url('/')?>?ci=' + xjson[data].cedula_usuario + '&action=edit"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar</a> ' + "</th>" +
-                    "<th>" + '<a class = "btn btn-danger" onclick="return validate_delete();"  href = "<?php echo base_url('/')?>?ci=' + xjson[data].cedula_usuario + '&action=remove"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Eliminar</a> ' + "</th></tr>" ;
-                }
-                $("#tablecont").html(_html); 
-            }
-        </script>
+        <div id = "mpag" >
+        </div>
     </div>
 </div>
+<script type = "text/javascript">
+
+function validate_delete()
+{
+    return confirm("Desea eliminar este registro?");
+}
+
+var m_json = "";
+
+$(document).ready(function()
+{
+    updatetable();
+});
+$("#ch_name").change(function()
+{
+    updatetable();
+});
+$("#ch_ci").change(function()
+{
+    updatetable();
+});
+$("#txt_name").keyup(function()
+{
+    updatetable();
+});
+$("#txt_ci").keyup(function()
+{
+    updatetable();
+});
+
+function updatetable()
+{
+    $.ajax
+    (
+        {
+            type : "POST",
+            url: "<?php echo base_url('index.php/user/getalltecs'); ?>",
+            datatype : 'json', 
+            data: {} ,
+            success:
+            function (res)
+            {
+                m_json = JSON.parse(res);
+                var name_checked = $("#ch_name").is(':checked');
+                if (name_checked)
+                {
+                    var f_arr = m_json.filter(function (value)
+                    {
+                        var filtrer_name = $("#txt_name").val();
+                        var x = value.nombre.includes(filtrer_name);
+                        return x ;
+                    });
+                    m_json = f_arr;
+                }
+                var ci_checked = $("#ch_ci").is(":checked");
+                if (ci_checked)
+                {
+                    var m_arr = m_json.filter(function (value)
+                    {
+                        var filter_ci = $("#txt_ci").val();
+                        var x = value.cedula_usuario.includes(filter_ci);
+                        return x;
+                    });
+                    m_json = m_arr;
+                }
+                populate(m_json);
+            }
+        }
+    );
+}
+function populate(xjson , isfiltrer)
+{
+    updatehtml(xjson);
+    $("#mpag").pagination
+    ({
+            dataSource: xjson,
+            pageSize : 20,
+            callback: function(data, pagination) 
+            {
+               xjson = data;
+               updatehtml(xjson);
+            }
+    });
+}
+function updatehtml(xjson)
+{
+    var _html = "" ;
+    for (data in xjson)
+    {
+        _html += "<tr><th>" + xjson[data].cedula_usuario + "</th>" + 
+        "<th>" + xjson[data].nombre + "</th>" +
+        "<th>" + xjson[data].correo + "</th>" +
+        "<th>" + '<a class = "btn btn-primary" href = "<?php echo base_url('/')?>?ci=' + xjson[data].cedula_usuario + '&action=edit"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar</a> ' + "</th>" +
+        "<th>" + '<a class = "btn btn-danger" onclick="return validate_delete();"  href = "<?php echo base_url('/')?>?ci=' + xjson[data].cedula_usuario + '&action=remove"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Eliminar</a> ' + "</th></tr>" ;
+    }
+    $("#tablecont").html(_html); 
+}
+</script>
