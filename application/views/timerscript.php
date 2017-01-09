d78c804a6df89ea6d144977b5961e62cdb7a2942
pedro15 <pedrojosuedm@hotmail.com>
2017-01-08 22:48:40 -0400

    Corregido sistema de expiracion de sesiones

--- application/views/timerscript.php
+++ application/views/timerscript.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No esta permitido el acceso directo al script.');
 ?>
 <script type = "text/javascript" >
 // Tiempo de Expiracion en minutos:
-var wait=   1  ;
+var wait=   15  ;
 // ----
-function resetTimer(){timer=window.setTimeout(logout,6e4*wait)}function logout(){var a='<?php echo base_url("index.php/user/logout"); ?>';window.location.href=a}var timer;$(document).ready(function(){resetTimer()}),$(document).keypress(function(){resetTimer()}),$(document).mousemove(function(){resetTimer()});
+var timer;function resetTimer(){window.clearTimeout(timer);timer=window.setTimeout(logout,6e4*wait);}function logout(){var a='<?php echo base_url("index.php/user/logout"); ?>';window.location.href=a}var timer;$(document).ready(function(){resetTimer()}),$(document).keypress(function(){resetTimer()}),$(document).mousemove(function(){resetTimer()});
 </script>
